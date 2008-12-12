a5dc2bb5ee0636954475baba587f237043ccd924
Klaus Ita <klaus@worstofall.com>
2008-12-12 18:01:33 +0100

    typo

--- lib/Bitmask/Data.pm
+++ lib/Bitmask/Data.pm
@@ -574,7 +574,7 @@ sub length {
 
     $bm->first()
     
-Returns the first set value (the order of the values is determied by the 
+Returns the first set value (the order of the values is determined by the 
 sequence of the addition)
 
 =cut
@@ -933,4 +933,4 @@ LICENSE file included with this module.
 
 =cut
 
-'0b000000000001';
+0b000000000001;
