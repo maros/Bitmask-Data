854e616925bcc42a1caa690e4dc9537cbcbe5375
Maros Kollar <maros@k-1.com>
2010-10-29 18:14:33 +0200

    Better documentation

--- lib/Bitmask/Data.pm
+++ lib/Bitmask/Data.pm
@@ -61,7 +61,7 @@ overloaded arithmetic and bit operators.
 
 Bitmask::Data does not store bitmasks as integers internally, but as 
 strings conststing of \0 and \1, hence makinging unlimited length bitmasks
-possible (32-bit perl can handle integer bitmasks only up to 40 bits).
+possible.
 
 =head1 METHODS
 
@@ -84,7 +84,7 @@ Default: undef
 
 =head3 bitmask_lazyinit
 
-If true value that disables warnings for lazy initialization. (Lazy 
+If true warning for lazy initialization are disabled. (Lazy 
 initialization = call of init without bitmask bit values).
 
 Default: 0
@@ -358,7 +358,7 @@ sub bit2bit {
 
     my $bitmask_string = CLASS->any2bitmask(ANYTHING);
 
-Helper method that tries to turn a data into the internal bitmask 
+Helper method that tries to turn arbitrary arguments into the internal bitmask
 representation. This method can hanle
 
 =over
@@ -467,7 +467,7 @@ Bitmask::Data uses overload by default.
 =item * Numeric context
 
 Returns bitmask integer value (see L<integer> method). For large bitmasks 
-(> 40 bits) this will allways be a L<Math::BigInt> object (hence using this
+(> 40 bits) this will always be a L<Math::BigInt> object (hence using this
 method is not recommended).
 
 =item * Scalar context
@@ -478,7 +478,7 @@ Returns bitmask string representation (see L<string> method)
 
 Works like 'has_any'
 
-=item * Smartmatch
+=item * ~~
 
 Works like L<has_any>.
 
@@ -521,9 +521,9 @@ Performs the bitwise operations on the current bitmask object.
     my $bm = MyBitmask->new([32, 'value1', 0b00010000010000]);
 
 Create a new bitmask object. You can supply almost any combination of 
-ARRAYREFS, bits, Bitmask::Data objects, Math::BigInt objects, bitmasks and 
-values, even mix different types. See L<any2bitmask> for details on possible
-formats.
+ARRAYREFS, bits, integers, Bitmask::Data objects, Math::BigInt objects, 
+bitmasks and values, even mix different types. See L<any2bitmask> for details 
+on possible formats.
 
 =cut
 
@@ -964,7 +964,7 @@ bitmask and not a value)
 
 Bitmask::Data adds a considerable processing overhead to bitmask 
 manipulations. If you either don't need the extra comfort or use 
-bitmasks with less that 40 bits that you should consider using just the perl 
+bitmasks with less that 32 bits that you should consider using just the perl 
 built in bit operators on simple integer values.
 
 =head1 SUBCLASSING
@@ -984,7 +984,7 @@ Bitmask::Data was designed to be subclassed.
 
 =head1 WORKING WITH DATABASES
 
-This module comes with support for POSTGRESQL databases (patches for other
+This module comes with support for PostgreSQL databases (patches for other
 database vendors are welcome). 
 
 First you need to create the correct column types:
@@ -1009,8 +1009,8 @@ This module provides three convenient methods to work with databases:
 
 =back
 
-If you are working with l<DBIx::Class> you might also install de- and 
-inflators for Bitmask::Data objects:
+If you are working with l<DBIx::Class> you might also install in- and 
+deflators for Bitmask::Data objects:
 
     __PACKAGE__->inflate_column('fieldname',{
         inflate => sub {
